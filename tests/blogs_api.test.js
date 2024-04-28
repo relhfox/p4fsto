6580abdd71e204aaa5c675dae91dc4c4cccd44ce
Max Rustle <csmoke.mk@gmail.com>
2024-04-28 17:27:19 +0100

    exercise 4-8 completed

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,27 @@
+const { test, describe, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+
+describe('get request', () => {
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('there are three blogs returned', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.body.length, 3)
+    })
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
