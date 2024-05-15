2aac02b92c7d741e0ef8d2804cf814a82f47da2a
Max Rustle <csmoke.mk@gmail.com>
2024-05-15 17:24:20 +0100

    exercise 4-10 completed

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -1,10 +1,40 @@
-const { test, describe, after } = require('node:test')
+const { test, describe, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+    },
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5
+    },
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (let blog of initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
 
 
 describe('get request', () => {
@@ -18,12 +48,35 @@ describe('get request', () => {
 
     test('there are three blogs returned', async () => {
         const response = await api.get('/api/blogs')
-        assert.strictEqual(response.body.length, 3)
+        assert.strictEqual(response.body.length, initialBlogs.length)
     })
 
     test('the id property without low dash', async () => {
         const response = await api.get('/api/blogs')
         assert.strictEqual(response.body[0].hasOwnProperty('id'), true)
+    })
+})
+
+describe('post request', () => {
+
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: "The one is being tested",
+            author: "Great Genius",
+            url: "https://fortest.ua/",
+            likes: 11
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(r => r.title)
+
+        assert.strictEqual(response.body.length, initialBlogs.length + 1)
+        assert(contents.includes('The one is being tested'))
     })
 })
 
